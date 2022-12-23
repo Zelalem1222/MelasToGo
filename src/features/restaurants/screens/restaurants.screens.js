ea40f35413cb4b86d308a61cb7e7bdd8ff168cf3
Zelalem1222 <zelalemfissha394@gmail.com>
2022-12-23 15:35:32 +0300

    Modify restaurant screen

--- src/features/restaurants/screens/restaurants.screens.js
+++ src/features/restaurants/screens/restaurants.screens.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native';
 import { Searchbar } from 'react-native-paper'
-import { StyleSheet, Text, View , StatusBar , SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View , StatusBar , SafeAreaView, FlatList } from 'react-native';
 import { RestaurantInfoCard } from '../components/restaurants-info-card.components';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
 const RestaurantListContainer = styled.View`
      flex : 1
@@ -22,9 +23,21 @@ export const RestaurantsScreen = () => {
       <SearchContainer >
       <Searchbar placeholder='Search' />
       </SearchContainer>
-      <RestaurantListContainer>
-        <RestaurantInfoCard />
-      </RestaurantListContainer>
+
+      <FlatList 
+       data={[{name: 1} , { name:2 } ,{name: 3} , { name:4 } , {name: 5} , { name:6 } , {name: 7} , { name:8 } , {name: 9} , { name:10 } ,{name: 11} , { name:12 } ,  ]}
+       renderItem={() =>
+        <>
+        <Spacer position='bottom' size='large'>
+       <RestaurantInfoCard />
+       </Spacer>
+       </> 
+       }
+       keyExtractor={(item ) => item.name}
+       contentContainerStyle={{ padding: 16 }}
+      />
+        
+
     </SafeArea>
   )
 }
