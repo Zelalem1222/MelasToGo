97ff4308e24b18068a8c8b005d1060cb55a84a22
Zelalem1222 <zelalemfissha394@gmail.com>
2022-12-31 11:38:38 +0300

    Created a restaurant context

---
+++ src/services/restaurants/restaurants.context.js
@@ -0,0 +1,54 @@
+import React,{ useState , createContext , useEffect , useMemo, useContext } from "react";
+import { LocationContext } from "../locations/location.context";
+
+
+import { restaurantsRequest , restaurantTransfromed } from "./restaurants.service";
+
+export const RestaurantsContext = createContext()
+
+export const RestaurantsContextProvider = ({children}) => {
+    const [restaurants , setRestaurants] = useState([])
+    const [isLoading , setIsLoading] = useState(false)
+    const [error , setError] = useState(null)
+    
+    const { location } = useContext(LocationContext)
+
+    const retriveRestaurants = (loc) => {
+        setIsLoading(true)
+        setRestaurants([])
+
+
+        setTimeout(() => {
+         restaurantsRequest(loc)
+         .then(restaurantTransfromed)
+         .then((results) => {
+            setRestaurants(results)
+            setIsLoading(false)
+         })
+         .catch((err) => { 
+            setError(err)
+            setIsLoading(false)
+        })
+        } , 2000)
+    }
+    
+
+   useEffect(() => {
+    if(location){
+        const locationString = `${location.lat},${location.lng}`
+        console.log(locationString)
+        retriveRestaurants(locationString)
+    }
+   } , [location])
+
+
+    return (
+        <RestaurantsContext.Provider value={{
+            restaurants,
+            isLoading,
+            error
+        }}>
+            {children}
+        </RestaurantsContext.Provider>
+    )
+}
