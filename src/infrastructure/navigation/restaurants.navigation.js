4c450d757d40da90e32c99ce4d4c547d3a5258dc
Zelalem1222 <zelalemfissha394@gmail.com>
2023-01-23 09:59:58 +0300

    modify restaurant navigation

--- src/infrastructure/navigation/restaurants.navigation.js
+++ src/infrastructure/navigation/restaurants.navigation.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Text } from 'react-native'
-import { createStackNavigator } from "@react-navigation/stack"
+import { createStackNavigator , TransitionPresets } from "@react-navigation/stack"
 import { RestaurantsScreen } from '../../features/restaurants/screens/restaurants.screens';
+import { RestaurantDetailScreen } from '../../features/restaurants/screens/restaurantDetail.screen';
 const ReastaurantStack = createStackNavigator()
 
 export const RestaurantsNavigator = () => {
     return (
-   <ReastaurantStack.Navigator headerMode='none'>
+   <ReastaurantStack.Navigator headerMode='none' screenOptions={{
+      ...TransitionPresets.ModalSlideFromBottomIOS
+   }}>
      <ReastaurantStack.Screen 
        name='Reastaurants'
        component={RestaurantsScreen}
      />
       <ReastaurantStack.Screen 
        name='ReastaurantDetail'
-       component={() => <Text>Resataurant Detail</Text>}
+       component={RestaurantDetailScreen}
      />
    </ReastaurantStack.Navigator>
     )
