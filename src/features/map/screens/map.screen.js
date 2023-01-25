d07dfa55f32a4b146bf2889cca6b3b89527a514f
Zelalem1222 <zelalemfissha394@gmail.com>
2023-01-25 17:53:40 +0300

    Created map screens

---
+++ src/features/map/screens/map.screen.js
@@ -0,0 +1,61 @@
+import React, {useContext, useEffect , useState} from "react";
+import MapView from "react-native-maps";
+import { Marker , Callout } from "react-native-maps";
+import styled from "styled-components/native";
+
+
+import { LocationContext } from "../../../services/locations/location.context";
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context'
+
+import { MapCallout } from '../components/map-callout'
+
+import { Search } from "../components/search.component";
+
+const Map = styled(MapView)`
+    height: 100%;
+    width: 100%;
+`;
+
+const someText = styled.Text``
+
+export const MapScreen = () => {
+    const [latDelta , setLatDelta] = useState(0);
+    const { location } = useContext(LocationContext);
+    const { restaurants = [] } = useContext(RestaurantsContext);
+    const { lat, lng , viewport } = location;
+    console.log(restaurants)
+
+    useEffect(() => {
+       const northeastLat = viewport.northeast.lat;
+       const southwestLat = viewport.southwest.lat;
+            setLatDelta(northeastLat - southwestLat);
+    } , [location , viewport])
+    return (
+    <>
+    <Search />
+    <Map
+     region={{ 
+            latitude: lat,
+            longitude: lng,
+            latitudeDelta: latDelta,
+            longitudeDelta: 0.03
+    }}
+    >
+        {restaurants.map((restaurant) => {
+            return <Marker  
+            key={restaurant.name}
+            title={restaurant.name}
+            coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng
+            }}
+            >
+                <Callout>
+                    <MapCallout restaurant={restaurant} />
+                </Callout>
+            </Marker>
+        })}
+    </Map>
+    </>
+    )
+}
