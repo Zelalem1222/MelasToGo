d10febae646bf2ffbc2901a770dc0f3e958af1fe
Zelalem1222 <zelalemfissha394@gmail.com>
2023-01-25 17:51:56 +0300

    Modify search component

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,4 +1,4 @@
-import React , {useContext , useState} from "react";
+import React , {useContext , useEffect, useState} from "react";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
 
@@ -6,12 +6,15 @@ import { LocationContext } from "../../../services/locations/location.context";
 
 const SearchContainer = styled.View`
     padding-horizontal: ${props => props.theme.space[3]}
+    margin-top: ${props => props.theme.space[3]}
 `
 
 export const Search = () => {
     const { keyword , search } = useContext(LocationContext)
     const [searchKeyword , setSearchKeyword] = useState(keyword)
-    
+    useEffect(() => {
+     setSearchKeyword(keyword)
+    } , [keyword])
     return (
         <SearchContainer >
         <Searchbar 
@@ -24,6 +27,6 @@ export const Search = () => {
             setSearchKeyword(text)
         }}
         />
-        </SearchContainer>
+         </SearchContainer>
     )
 }
