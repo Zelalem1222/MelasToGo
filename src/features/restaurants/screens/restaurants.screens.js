b402a6a68f2191de2bb007ee5f14b77cf4295ec3
Zelalem1222 <zelalemfissha394@gmail.com>
2023-02-23 11:10:15 +0300

    Modify restaurant screen

--- src/features/restaurants/screens/restaurants.screens.js
+++ src/features/restaurants/screens/restaurants.screens.js
@@ -6,6 +6,7 @@ import {  FlatList , TouchableOpacity, View } from 'react-native';
 import { SafeArea } from '../../../components/utility/safeArea.component';
 import { RestaurantInfoCard } from '../components/restaurants-info-card.components';
 import { Spacer } from '../../../components/spacer/spacer.component';
+import { FavouriteContext } from '../../../services/favourites/favourites.context';
 
 import { FavouritesBar } from '../../../components/favourite/favourites-bar.component';
 
@@ -33,6 +34,7 @@ const LoadingContainer = styled.View`
 export const RestaurantsScreen = ({ navigation }) => {
   const {restaurants , isLoading , error} = useContext(RestaurantsContext)
  const [isToggled , setIsToggled] = useState(false)
+ const { favourites } = useContext(FavouriteContext)
   return (
     
     <SafeArea>
@@ -42,7 +44,7 @@ export const RestaurantsScreen = ({ navigation }) => {
       <Loading size={50} animating={true} color={MD2Colors.blue300}/>  
       </LoadingContainer>}
       <Search isFavouritesToggle={isToggled} onFavouritesToggle={() => setIsToggled(!isToggled)} />
-      {isToggled && <FavouritesBar />}
+      {isToggled && <FavouritesBar favourites={favourites} onNavigate={navigation.navigate}/>}
       <RestaurantList 
        data={restaurants}
        renderItem={({ item }) =>{
