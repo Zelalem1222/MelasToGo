18231c576243693c3b8b3957a55e2c96c6590a73
Zelalem1222 <zelalemfissha394@gmail.com>
2023-02-23 10:26:51 +0300

    Created a new component for favouries bar

---
+++ src/components/favourite/favourites-bar.component.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View , Text } from 'react-native'
+
+export const FavouritesBar = () => {
+    return (
+        <View>
+            <Text>Favourites Bar</Text>
+        </View>
+    )
+}
