a3497cb7d747b8b2ee41e311e618bd25b43f702d
Zelalem1222 <zelalemfissha394@gmail.com>
2023-02-27 12:45:18 +0300

    Modify favourite component

--- src/components/favourite/favourite.components.js
+++ src/components/favourite/favourite.components.js
@@ -15,7 +15,7 @@ const FavouriteButton = styled(TouchableOpacity)`
 export const Favourite = ({ restaurant }) => {
     const { favourites , addToFavourites , removeFromFavourites } = useContext(FavouriteContext)
      const handlePress = () => {
-        console.log(restaurant)
+       
      }
      const isFavourite = favourites.find((r) => r.placeId === restaurant.placeId)
 
