8fc66adc6841488670bc7e2b214f57245847b7bd
Zelalem1222 <zelalemfissha394@gmail.com>
2023-03-09 09:24:47 +0300

    Inject firebase to the app

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 
 import { ThemeProvider } from 'styled-components/native';
+import * as firebase from "firebase"
 
 
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
@@ -11,6 +12,22 @@ import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 
 import { theme } from './src/infrastructure/theme';
 import { Navigation } from './src/infrastructure/navigation';
+
+
+const firebaseConfig = {
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
+};
+
+firebase.initializeApp(firebaseConfig)
+
+
 export default function App() {
 
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
