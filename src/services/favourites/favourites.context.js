1f49794e06619ee7e9fcb38f239417b320626f89
Zelalem1222 <zelalemfissha394@gmail.com>
2023-03-09 09:27:06 +0300

    Modify favourites context to store favourites locally

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,11 +1,39 @@
-import React , { createContext , useState } from 'react'
-import { Children } from 'react/cjs/react.production.min';
+import React , { createContext , useState , useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const FavouriteContext = createContext();
 
 export const FavouriteContextProvider = ({ children }) => {
 
   const [favourites , setFavourites] = useState([])
+
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value)
+      await AsyncStorage.setItem('@favourites', jsonValue)
+    } catch (e) {
+      console.log('error saving', e)
+    }
+  }
+
+  const loadFavourites = async () => {
+    try {
+       const value = await AsyncStorage.getItem('@favourites')
+        if(value !== null) {
+          setFavourites(JSON.parse(value))
+        }
+    } catch(e) {
+      console.log('error loading', e)
+    }
+  }
+
+  useEffect(() => {
+    loadFavourites()
+  }, [])
+
+  useEffect(() => {
+     saveFavourites(favourites)
+  } , [favourites])
 
   const add = (restaurant) => {
     setFavourites([...favourites , restaurant])
