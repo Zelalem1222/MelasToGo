c885a1e824b56bfd8a4af4aa6e564281a27e60cb
Zelalem1222 <zelalemfissha394@gmail.com>
2023-03-16 08:36:16 +0300

    Test the authentication

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-
+import React, { useState , useEffect } from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import * as firebase from "firebase"
-
 
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
 import { LocationContextProvider } from './src/services/locations/location.context';
@@ -15,20 +14,32 @@ import { Navigation } from './src/infrastructure/navigation';
 
 
 const firebaseConfig = {
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+    apiKey: "AIzaSyDmLsga9EmRRyU-mlMwnf5-6U0QRwuH5kI",
+    authDomain: "mealstogo-92686.firebaseapp.com",
+    projectId: "mealstogo-92686",
+    storageBucket: "mealstogo-92686.appspot.com",
+    messagingSenderId: "210812673643",
+    appId: "1:210812673643:web:d8d6ff06e7c655d2205b2e"
 };
 
 firebase.initializeApp(firebaseConfig)
 
 
 export default function App() {
+   const [ isAuthenticated , setIsAutehenticated ] = useState(false)
+
+   useEffect(() => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword("email@gmal.com" , "zola123")
+      .then((user) => {
+        console.log(user)
+        setIsAutehenticated(true)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+   }, [])
 
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
   const [ latoLoaded ] = useLato({Lato_400Regular })
