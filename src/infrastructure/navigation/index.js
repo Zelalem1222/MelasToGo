f2146ab9aaee6b4722d053b385315f444e94c9cc
Zelalem1222 <zelalemfissha394@gmail.com>
2023-07-14 05:40:15 +0300

    Modify the index navigator

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -2,16 +2,16 @@ import React, {useContext} from "react";
 
 import { NavigationContainer } from '@react-navigation/native';
 import { AppNavigator } from "./app.navigator";
-import { AuthenticationContext } from "../../services/authentication/authentication.context";
-import { AccountNavigator } from "./account.navigator";
+// import { AuthenticationContext } from "../../services/authentication/authentication.context";
+// import { AccountNavigator } from "./account.navigator";
 
 export const Navigation = () => {
    
-    const { isAuthenticated } = useContext(AuthenticationContext)
+    // const { isAuthenticated } = useContext(AuthenticationContext)
 
     return (
         <NavigationContainer>
-          {isAuthenticated ? (<AppNavigator />) : (<AccountNavigator />)}
+         <AppNavigator />
         </NavigationContainer>
 
 )}
