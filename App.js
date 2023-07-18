04cbbdfe514a5fb964e4efdf42d9fd5f5287d111
Zelalem1222 <zelalemfissha394@gmail.com>
2023-07-18 16:25:18 +0300

    Remove unwanted code

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
-import React, { useState , useEffect } from 'react';
+import React from 'react';
 import { ThemeProvider } from 'styled-components/native';
-// import { initializeApp } from 'firebase/app';
-// import { getAuth , signInWithEmailAndPassword } from 'firebase/auth'
 
 
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
 import { LocationContextProvider } from './src/services/locations/location.context';
 import { FavouriteContextProvider } from './src/services/favourites/favourites.context';
-// import { AuthenticationContextProvider } from './src/services/authentication/authentication.context';
+
 
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
@@ -16,34 +14,8 @@ import { theme } from './src/infrastructure/theme';
 import { Navigation } from './src/infrastructure/navigation';
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDmLsga9EmRRyU-mlMwnf5-6U0QRwuH5kI",
-//     authDomain: "mealstogo-92686.firebaseapp.com",
-//     projectId: "mealstogo-92686",
-//     storageBucket: "mealstogo-92686.appspot.com",
-//     messagingSenderId: "210812673643",
-//     appId: "1:210812673643:web:d8d6ff06e7c655d2205b2e"
-// };
-
-// if(!firebase.apps.)
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth()
-
 
 export default function App() {
-  //  const [ isAuthenticated , setIsAutehenticated ] = useState(false)
-
-  //  useEffect(() => {
-  //     signInWithEmailAndPassword(auth, "email@gmal.com" , "zola123")
-  //     .then((user) => {
-  //       console.log(user)
-  //       setIsAutehenticated(true)
-  //     })
-  //     .catch((e) => {
-  //       console.log(e)
-  //     })
-  //  }, [])
 
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
   const [ latoLoaded ] = useLato({Lato_400Regular })
