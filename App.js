161d8182a9b1f905d030d0dcb5f3fc8930040f26
Zelalem1222 <zelalemfissha394@gmail.com>
2023-07-21 13:57:27 +0300

    Modify App.j

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { useEffect , useState } from 'react';
 import { ThemeProvider } from 'styled-components/native';
 
 
@@ -15,10 +15,25 @@ import { Navigation } from './src/infrastructure/navigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './src/Login';
+import List from './src/list';
+import Detail from './src/details';
+
+import { FIREBASE_AUTH } from './firebase';
+import { User, onAuthStateChanged } from 'firebase/auth';
+
 
 
 const Stack = createStackNavigator();
+const InsideStack = createStackNavigator();
 
+function InsideLayout() {
+  return (
+    <InsideStack.Navigator>
+       
+        <InsideStack.Screen name='Home' component={List} options={{ headerShown: false }}/>
+        <InsideStack.Screen name='Details' component={Detail} options={{ headerShown: false }}/>
+      </InsideStack.Navigator>
+  )}
 export default function App() {
 
   // const [oswaldLoaded] = useOswald({ Oswald_400Regular });
@@ -27,12 +42,25 @@ export default function App() {
   // if(!oswaldLoaded && !latoLoaded){
   //   return null
   // }
+ 
+
+  const [user , setUser] = useState(null)
   
+  useEffect(() => {
+    // const user = firebase.auth().currentUser;
+    firebase.auth().onAuthStateChanged( (user) => {
+      console.log('user', user)
+      setUser(user)
+    })
+  }, [])
 
   return (
     <NavigationContainer>
        <Stack.Navigator initialRouteName="Loading">
-        <Stack.Screen name='Login' component={Login} options={{ headerShown: false }}/>
+        { user ? (<Stack.Screen name='Inside' component={InsideLayout} options={{ headerShown: false }}/> ) :
+         (<Stack.Screen name='Login' component={Login} options={{ headerShown: false }}/> )
+        }
+       
         </Stack.Navigator>
       </NavigationContainer>
   );
