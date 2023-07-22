caf9f7252482cc46f201121ad0d14064f3a33b5c
Zelalem1222 <zelalemfissha394@gmail.com>
2023-07-22 11:00:03 +0300

    Create a logout button

--- src/list.js
+++ src/list.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text , StyleSheet , Button } from 'react-native';
+import { NavigationProp } from '@react-navigation/native';
+import { FIREBASE_AUTH } from '../firebase';
 
 
-export default function List(){
+
+
+export default function List({navigation} ){
     return(
-        <View>
-            <Text>hello</Text>
+        <View style={styles.container}>
+            <Button onPress={() => navigation.navigate('Details')} title='Open Details' />
+            <Button onPress={() => FIREBASE_AUTH.signOut()} title='Logut' />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
