5c3cb646c6b42ca83a3b5cc77d60c280f864d05a
Zelalem1222 <zelalemfissha394@gmail.com>
2024-03-28 08:26:44 +0300

    Modify account screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Spacer } from '../../../components/spacer/spacer.component'
 
-import { AccountBackground } from "../components/account.styles";
+
+import { AccountBackground , AccountCover , AccountContainer , AuthButton } from "../components/account.styles";
 
 export const AccountScreen = () => {
-    return <AccountBackground />
+    return <AccountBackground>
+          <AccountCover />
+          <AccountContainer>
+          <Spacer size="large">
+          <AuthButton icon="lock-open-outline" mode="contained" onPress={() => console.log('Pressed')}>
+             Login
+        </AuthButton>
+        </Spacer>
+        <AuthButton icon="lock-open-outline" mode="contained" onPress={() => console.log('Pressed')}>
+             Register
+        </AuthButton>
+        
+          </AccountContainer>
+    </AccountBackground>
 }
