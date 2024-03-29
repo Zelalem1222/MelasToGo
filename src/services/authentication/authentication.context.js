2071388e9121bc7f8d9e07bcd1e0ac7cf654c105
Zelalem1222 <zelalemfissha394@gmail.com>
2024-03-29 08:52:37 +0300

    Modify authentication context

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,24 +1,27 @@
-import React , { useState , createContext } from "react";
-import firebase from 'firebase/app';
+import React , { useState , createContext  ,useRef } from "react";
+import { getAuth } from "firebase/auth";
+import {loginRequest} from './authentication.service';
+
 
 export const AuthenticationContext = createContext();
-import {loginRequest} from './authentication.service';
+
 
 export const AuthenticationContextProvider = ({ children }) => {
        const [isLoading , setIsLoading] = useState(false);
        const [user , setUser] = useState(null);
        const [error , setError] = useState(null);
+       const auth = useRef(getAuth()).current;
 
         const onLogin = (email , password) => {
             setIsLoading(true);
-            loginRequest(email , password)
-            .then((u) => {
+            loginRequest(auth, email , password)
+              .then((u) => {
                 setUser(u)
                 setIsLoading(false)
-            })
-            .catch((e) => {
+              })
+              .catch((e) => {
                 setIsLoading(false)
-                setError(e)
+                setError(e.toString());
             })
         }
 
@@ -26,10 +29,11 @@ export const AuthenticationContextProvider = ({ children }) => {
        return ((
           <AuthenticationContext.Provider
            value={{
+            isAuthenticated: !!user,
             user,
             isLoading,
             error,
-            onLogin,
+            onLogin, 
            }}
           >
             {children}
