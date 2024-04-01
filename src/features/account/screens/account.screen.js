30bdd5aafd7d84df1f47c2bee8d67b958408f616
Zelalem1222 <zelalemfissha394@gmail.com>
2024-04-01 08:38:13 +0300

    Modify account screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Spacer } from '../../../components/spacer/spacer.component'
 
 
-import { AccountBackground , AccountCover , AccountContainer , AuthButton } from "../components/account.styles";
+import { AccountBackground , AccountCover , AccountContainer , AuthButton , Title } from "../components/account.styles";
 
 export const AccountScreen = ({navigation}) => {
     return <AccountBackground>
           <AccountCover />
           <AccountContainer>
-          
+          <Title>Meals To Go</Title>
           <AuthButton icon="lock-open-outline" mode="contained" onPress={() => navigation.navigate("Login")}>
              Login
         </AuthButton>
