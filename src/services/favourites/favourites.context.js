af7a1cbad1e3a20caa9c51322897888fb4e1bef2
Zelalem1222 <zelalemfissha394@gmail.com>
2024-07-19 06:36:34 +0300

    Modify favourites context

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,24 +1,27 @@
-import React , { createContext , useState , useEffect } from 'react'
+import React , { createContext , useState , useEffect , useContext } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AuthenticationContext } from '../authentication/authentication.context';
 
 export const FavouriteContext = createContext();
 
 export const FavouriteContextProvider = ({ children }) => {
-
+  const {user} = useContext(AuthenticationContext);
   const [favourites , setFavourites] = useState([])
 
-  const saveFavourites = async (value) => {
+  const saveFavourites = async (value, uid) => {
+    console.log(uid)
     try {
-      const jsonValue = JSON.stringify(value)
-      await AsyncStorage.setItem('@favourites', jsonValue)
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem(`@favourites-${uid}`, jsonValue);
     } catch (e) {
-      console.log('error saving', e)
+      console.log("error storing", e);
     }
-  }
+  };
 
-  const loadFavourites = async () => {
+  const loadFavourites = async (uid) => {
+    
     try {
-       const value = await AsyncStorage.getItem('@favourites')
+       const value = await AsyncStorage.getItem(`@favourites-${uid}`)
         if(value !== null) {
           setFavourites(JSON.parse(value))
         }
@@ -28,12 +31,17 @@ export const FavouriteContextProvider = ({ children }) => {
   }
 
   useEffect(() => {
-    loadFavourites()
-  }, [])
+    if(user && user.uid){
+      loadFavourites(user.uid)
+    }
+  }, [user])
 
   useEffect(() => {
-     saveFavourites(favourites)
-  } , [favourites])
+    if (user && user.uid && favourites.length) {
+      console.log(user)
+      saveFavourites(favourites, user.uid);
+    }
+  }, [favourites, user]);
 
   const add = (restaurant) => {
     setFavourites([...favourites , restaurant])
