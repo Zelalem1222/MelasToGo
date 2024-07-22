28d33bbcbd3c6d531d0bea9da29e96c7d3df2a41
Zelalem1222 <zelalemfissha394@gmail.com>
2024-07-22 04:12:02 +0300

    Modify account screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,12 +1,22 @@
 import React from "react";
+import LottieView from "lottie-react-native";
 import { Spacer } from '../../../components/spacer/spacer.component'
 
 
-import { AccountBackground , AccountCover , AccountContainer , AuthButton , Title } from "../components/account.styles";
+import { AccountBackground , AccountCover , AccountContainer , AuthButton , Title , AnimationWrapper } from "../components/account.styles";
 
 export const AccountScreen = ({navigation}) => {
     return <AccountBackground>
           <AccountCover />
+          <AnimationWrapper>
+          <LottieView
+          key="animation"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/watermelon.json")}
+        />
+       </AnimationWrapper>
           <AccountContainer>
           <Title>Meals To Go</Title>
           <AuthButton icon="lock-open-outline" mode="contained" onPress={() => navigation.navigate("Login")}>
