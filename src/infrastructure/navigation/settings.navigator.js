a337aa9cf3869186244ba3231a59d96106d70d4a
Zelalem1222 <zelalemfissha394@gmail.com>
2024-07-22 06:22:58 +0300

    Modify settings navigator

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import { createStackNavigator, CardStyleInterpolators } from "@react-navigation/stack";
 import { SettingScreen } from "../../features/setting/screens/settings.screens";
-
+import { FavouritesScreen } from "../../features/setting/screens/favourites.screens";
 const SettingsStack = createStackNavigator();
 
 export const SettingsNavigator = ({route , navigation})=> {
     return (
         <SettingsStack.Navigator
-        headerMode="Screen"
+        
         screenOptions={{
             CardStyleInterpolators: CardStyleInterpolators.forHorizontalIOS
         }}
@@ -22,7 +22,7 @@ export const SettingsNavigator = ({route , navigation})=> {
             />
             <SettingsStack.Screen 
              name="Favourites"
-             component={() => null}
+             component={FavouritesScreen}
             />
         </SettingsStack.Navigator>
     )
