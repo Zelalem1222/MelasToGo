bf68a27b5f343bd6a994a1640a01d1ba6fd5893f
Zelalem1222 <zelalemfissha394@gmail.com>
2024-07-26 05:18:08 +0300

    Modify settings cereen

--- src/features/setting/screens/settings.screens.js
+++ src/features/setting/screens/settings.screens.js
@@ -1,5 +1,6 @@
 import React , {useContext} from 'react';
 import styled from 'styled-components/native';
+import { TouchableOpacity } from 'react-native';
 
 import { List , Avatar } from 'react-native-paper';
 
@@ -21,8 +22,11 @@ export const SettingScreen = ({navigation}) => {
     const {onLogout , user} = useContext(AuthenticationContext);
     return (
       <SafeArea>
+        
         <AvatarContainer>
+          <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
         <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        </TouchableOpacity>
         <Spacer position="top" size="large">
           <Text variant="label">{user.email}</Text>
         </Spacer>
