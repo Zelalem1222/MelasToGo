07a92e63a5b540e4319f8412488d9345fe23d1a9
Zelalem1222 <zelalemfissha394@gmail.com>
2024-07-26 05:22:56 +0300

    Modify settings navigator

--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -3,6 +3,7 @@ import React from "react";
 import { createStackNavigator, CardStyleInterpolators } from "@react-navigation/stack";
 import { SettingScreen } from "../../features/setting/screens/settings.screens";
 import { FavouritesScreen } from "../../features/setting/screens/favourites.screens";
+import { CameraScreen } from "../../features/setting/screens/camera.screens";
 const SettingsStack = createStackNavigator();
 
 export const SettingsNavigator = ({route , navigation})=> {
@@ -23,6 +24,10 @@ export const SettingsNavigator = ({route , navigation})=> {
             <SettingsStack.Screen 
              name="Favourites"
              component={FavouritesScreen}
+            />
+            <SettingsStack.Screen 
+             name="Camera"
+             component={CameraScreen}
             />
         </SettingsStack.Navigator>
     )
