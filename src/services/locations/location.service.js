90a3a86a895e51f5e03091f124ca063cd0b60f00
Zelalem1222 <zelalemfissha394@gmail.com>
2024-07-29 10:46:05 +0300

    Change the fecth type

--- src/services/locations/location.service.js
+++ src/services/locations/location.service.js
@@ -1,12 +1,15 @@
 import { Linking } from "react-native";
 import camelize from "camelize";
+import { locations } from "../../../functions/geocode/geocode.mock";
 
 
 export const locationRequest = (searchTerm ) => {
-    return  fetch(`http://127.0.0.1:5001/mealstogo-92686/us-central1/geocode?city=${searchTerm}`).then((res) => {
-        return res.json()
-    }).catch((err) => {
-        console.log(err)
+    return  new Promise((resolve , reject) => {
+        const locationMock = locations[searchTerm]
+        if(!locationMock){
+            reject("not found")
+        }
+        resolve(locationMock)
     })
 }
 
