2971fd8ae953213428c68d08263e6e3a40bca596
Zelalem1222 <zelalemfissha394@gmail.com>
2024-07-29 10:32:40 +0300

    Installed firebase functions

--- src/services/locations/location.service.js
+++ src/services/locations/location.service.js
@@ -1,17 +1,17 @@
+import { Linking } from "react-native";
 import camelize from "camelize";
-import { locations } from "./location.mock";
+
 
 export const locationRequest = (searchTerm ) => {
-    return new Promise((resolve , reject) => {
-        const locationMock = locations[searchTerm]
-        if(!locationMock){
-            reject('not found')
-        }
-        resolve(locationMock)
+    return  fetch(`http://127.0.0.1:5001/mealstogo-92686/us-central1/geocode?city=${searchTerm}`).then((res) => {
+        return res.json()
+    }).catch((err) => {
+        console.log(err)
     })
 }
 
 export const locationTransform = (result) => {
+    console.log(result)
     const formattedResponse = camelize(result)
     const { geometry = {} } = formattedResponse.results[0]
     const { lng , lat ,  } = geometry.location
