cb66c182558f1136d88c06ef04cfd73274884fec
Zelalem1222 <zelalemfissha394@gmail.com>
2024-08-01 16:56:59 +0300

    Modify restaurnat service

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,29 +1,29 @@
-import { mocks , mockImages } from './mock/index'
+import React from 'react'
+import { mocks  } from '../../../functions/places/mock'
 import camelize from 'camelize'
-import react from 'react'
+
 
 export const restaurantsRequest = (location) => {
-   return new Promise((resolve , reject) => {
-    const mock = mocks[location]
-    if(!mock){
-        reject('not found')
-    }
-    resolve(mock)
-   })
+    console.log("location -" , `${location}`)
+    console.log('mocks -' , mocks[`${location}`])
+    return new Promise((resolve, reject) => {
+        const mock = mocks[`${location}`]
+        if(!mock){
+            reject("Not found")
+        }
+        resolve(mock)
+    })
 }
 
 export const restaurantTransfromed = ({ results = [] }) => {
+    
 
    const mappedResults = results.map((restaurant) => {
-    restaurant.photos = restaurant.photos.map((p) => {
-        return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))]
-    })
     return {
         ...restaurant,
         isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
         isClosedTemporarily: restaurant.business_status === "OPERATIONAL",
         address: restaurant.vicinity
-        
     }
    })
 
