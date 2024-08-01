1749280d93bfb0119699f5a7f30d3c397d18ea8f
Zelalem1222 <zelalemfissha394@gmail.com>
2024-08-01 16:55:52 +0300

    Modify index.js

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,12 @@ const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 const { geocodeRequest } = require("./geocode")
+const { placesRequest } = require("./places")
 
 exports.geocode = onRequest((request, response) => {
     geocodeRequest(request , response);
+});
+
+exports.placesNearBy = onRequest((request, response) => {
+    placesRequest(request , response);
 });
