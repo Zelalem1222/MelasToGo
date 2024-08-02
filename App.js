92ff92c00e862d92a991b61ac20a925875b7ecd4
Zelalem1222 <zelalemfissha394@gmail.com>
2024-08-02 06:43:04 +0300

    Modify App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React , { useEffect , useState } from 'react';
 import { ThemeProvider } from 'styled-components/native';
 
 import { AuthenticationContextProvider } from './src/services/authentication/authentication.context';
@@ -8,7 +8,12 @@ import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 
 import { theme } from './src/infrastructure/theme';
 import { Navigation } from './src/infrastructure/navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+
+import { FIREBASE_AUTH } from './firebase';
+import { User, onAuthStateChanged } from 'firebase/auth';
 
 
 
