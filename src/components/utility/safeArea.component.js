6114dae9b9bcc2afc98c7b1b18a86232948a3d50
Zelalem1222 <zelalemfissha394@gmail.com>
2024-08-02 08:09:34 +0300

    Modify safe area

--- src/components/utility/safeArea.component.js
+++ src/components/utility/safeArea.component.js
@@ -4,5 +4,4 @@ import styled from 'styled-components';
 export const SafeArea = styled(SafeAreaView)`
       flex: 1
       background-color: ${props => props.theme.colors.bg.primary}
-      padding-top : ${StatusBar.currentHeight}px
 `
