ab09fe3f3bf00e063f8cf700bf0ad910b0281f65
Zelalem1222 <zelalemfissha394@gmail.com>
2024-08-06 06:05:34 +0300

    Created credit-card component

---
+++ src/features/checkout/components/credit-card.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { LiteCreditCardInput } from "react-native-credit-card-input";
+
+
+export const CreditCardInputs = () => {
+    const onChange = (formData) => {
+        const { values , status } = formData;
+        const isIncomplete = Object.values(status).includes("incomplete")
+        console.log(formData)
+        console.log(isIncomplete)
+    }
+
+    return (
+    <LiteCreditCardInput onChange={onChange} />
+     )
+};
