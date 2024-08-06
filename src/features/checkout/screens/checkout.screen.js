265a0290b91061cf81fc31a1a5efed0bf612641a
Zelalem1222 <zelalemfissha394@gmail.com>
2024-08-06 06:06:20 +0300

    Modified checkout screen

--- src/features/checkout/screens/checkout.screen.js
+++ src/features/checkout/screens/checkout.screen.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { SafeArea } from "../../../components/utility/safeArea.component";
+import { CreditCardInputs } from "../components/credit-card.component";
 
 
-export const CheckoutScreen = () => null;
+export const CheckoutScreen = () => <SafeArea><CreditCardInputs /></SafeArea>;
