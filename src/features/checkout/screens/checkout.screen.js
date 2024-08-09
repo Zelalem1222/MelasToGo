9ba7d311988577c459cafffa512b8af2466f844c
Zelalem1222 <zelalemfissha394@gmail.com>
2024-08-09 06:11:08 +0300

    modified checkout screen

--- src/features/checkout/screens/checkout.screen.js
+++ src/features/checkout/screens/checkout.screen.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React , {useContext} from "react";
+
+import { CartContext } from "../../../services/cart/cart.context";
 
 import { SafeArea } from "../../../components/utility/safeArea.component";
+import { Text } from "../../../components/typography/typography.component";
 import { CreditCardInputs } from "../components/credit-card.component";
 
+import { CartIconContainer , CartIcon } from "../components/checkout.styles.js"
 
-export const CheckoutScreen = () => <SafeArea><CreditCardInputs /></SafeArea>;
+export const CheckoutScreen = () => {
+    const { cart , restaurant } = useContext(CartContext)
+    if(!cart.length && !restaurant){
+        return (
+        <SafeArea>
+            <CartIconContainer>
+                <CartIcon icon="cart-off" />
+                <Text>Your cart is empty!</Text>
+            </CartIconContainer>
+        </SafeArea>
+        )
+    }
+return (
+<SafeArea>
+    <Text>{JSON.stringify(cart)}</Text>
+    <Text>restaurant : {JSON.stringify(restaurant)}</Text>
+    <CreditCardInputs />
+    </SafeArea>
+)
+}
