f638244f32eafe2cd086c2431ffbbb6b235804c4
Zelalem1222 <zelalemfissha394@gmail.com>
2024-08-09 06:10:38 +0300

    Modified credit-card component

--- src/features/checkout/components/credit-card.component.js
+++ src/features/checkout/components/credit-card.component.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { LiteCreditCardInput } from "react-native-credit-card-input";
-
-
-export const CreditCardInputs = () => {
-    const onChange = (formData) => {
+import { creatTokenRequest } from "../../../services/checkout/checkout.service";
+//
+export const CreditCardInputs = ({name = "zola"}) => {
+    const onChange = async (formData) => {
         const { values , status } = formData;
         const isIncomplete = Object.values(status).includes("incomplete")
+        console.log(isIncomplete)
+        const expiry = values.expiry.split("/")
+        const card = {
+            number: values.number,
+            exp_month: expiry[0],
+            exp_year: expiry[1],
+            cvc: values.cvc,
+            name: name
+        }
+        const info = await creatTokenRequest(card)
+        console.log(info);
     }
 
     return (
