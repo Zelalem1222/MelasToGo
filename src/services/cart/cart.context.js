c0ee524863933c601a7c9780f9e77b13e4972820
Zelalem1222 <zelalemfissha394@gmail.com>
2024-08-22 06:27:51 +0300

    Modified cart context

--- src/services/cart/cart.context.js
+++ src/services/cart/cart.context.js
@@ -16,12 +16,45 @@ export const CartContextProvider = ({children}) => {
         if(!restaurant || restaurant.placeId !==rest.placeId){
             setRestaurant(rest)
             setCart([item])
-            
         }else {
             setCart([...cart, item]);
         }
-        
     }
+
+    const saveCart = async(rst , crt , uid)=> {
+         try{
+            const jsonValue = JSON.stringify({restaurant: rst , cart:crt})
+            await AsyncStorage.setItem(`@cart-${uid}` , jsonValue)
+         }catch(e){
+           console.log("error storing" , e)
+         }
+    }
+
+    const loadCart = async (uid) => {
+        try{
+           
+          const value =  await AsyncStorage.getItem(`@cart-${uid}`)
+          if(value !==null){
+            const {restaurant:rst , cart: crt} = JSON.parse(value);
+            setCart(crt)
+            setRestaurant(rst)
+          }
+         }catch(e){
+           console.log("error storing" , e)
+         }
+    }
+
+    useEffect(() => {
+        if(user && user.uid){
+          loadCart( user.uid)
+        }
+      } , [ user])
+
+    useEffect(() => {
+      if(user && user.uid){
+         saveCart(restaurant , cart , user.uid)
+      }
+    } , [restaurant , cart , user])
     
     useEffect(() => {
        if(!cart.length){
