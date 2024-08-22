d7289143ee61816abe87e54ed1de6c5af6a44319
Zelalem1222 <zelalemfissha394@gmail.com>
2024-08-22 06:27:15 +0300

    Created payment processing style

--- src/features/checkout/components/checkout.styles.js
+++ src/features/checkout/components/checkout.styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components/native"
-import { Avatar, TextInput , Button } from "react-native-paper"
+import { Avatar, TextInput , Button, ActivityIndicator , MD2Colors } from "react-native-paper"
 import { colors } from "../../../infrastructure/theme/colors"
 
 
@@ -36,4 +36,15 @@ export const ClearButton = styled(Button).attrs({
     border-radius: 0
     align-self: center
     padding: ${props => props.theme.space[1]}
+`
+
+export const PaymentProceessing = styled(ActivityIndicator).attrs({
+    size: 128,
+    animating: true,
+    color: MD2Colors.blue200
+})`
+   position: absolute
+   top: 50%
+   left: 35%
+   z-index: 999
 `
